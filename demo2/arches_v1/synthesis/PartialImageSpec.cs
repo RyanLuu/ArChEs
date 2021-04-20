3e8e0760657dcfcbf49c17c53736cf77e82e607e
Harjas Monga <harjasmonga@gmail.com>
2021-04-20 19:11:06 -0400

    just a little more documentation

--- demo2/arches_v1/synthesis/PartialImageSpec.cs
+++ demo2/arches_v1/synthesis/PartialImageSpec.cs
@@ -15,17 +15,24 @@ namespace Arches {
 		public PartialImageSpec(IDictionary<State, IEnumerable<object>> examples): base(examples.Keys) {
 			this.examples = examples;
 		}
+
+		// compares an actual image to our partial image representation to check for a match
         protected override bool CorrectOnProvided(State state, object output)
         {
+			// extract the partial image
 			var space = (examples[state] as List<int[][]>)[0];
+			// the candidate image to see if it matches the partial image
 			var candidate = output as int[][];
 			for (int i = 0; i < space.Length; i++) {
 				for (int j = 0; j < space[0].Length; j++) {
+					// if the partial image is 10, then the candidate can be anything except 0
 					if (space[i][j] == 10) {
 						if (candidate[i][j] == 0) return false;
+					// if the partial image is -x, then then candidate can be anything except x
 					} else if (space[i][j] < 0) {
 						if (candidate[i][j] == -space[i][j]) return false;
 					} else {
+						// No more special color values, the candidate and the partial must match
 						if (space[i][j] != candidate[i][j]) return false;
 					}
 				}
